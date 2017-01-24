323f0849a7a658bb19cdb7a2540b5bb123f3548c
Klas Eskilson <klas.eskilson@gmail.com>
2017-01-24 15:33:37 -0800

    [Assets] Make sure assets are properly added

--- lib/administrate/field/belongs_to_search.rb
+++ lib/administrate/field/belongs_to_search.rb
@@ -5,6 +5,8 @@ module Administrate
   module Field
     class BelongsToSearch < Administrate::Field::BelongsTo
       class Engine < ::Rails::Engine
+        Engine.config.assets.precompile << 'belongs_to_search.js'
+        Administrate::Engine.add_javascript 'belongs_to_search.js'
       end
 
       def associated_resource_options
