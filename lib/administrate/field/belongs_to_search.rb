aeba4ba7b07d4bb5edee91237388ce63e8999a21
Klas Eskilson <klas.eskilson@gmail.com>
2017-03-03 16:38:39 -0800

    Add assets in initializer

--- lib/administrate/field/belongs_to_search.rb
+++ lib/administrate/field/belongs_to_search.rb
@@ -5,8 +5,10 @@ module Administrate
   module Field
     class BelongsToSearch < Administrate::Field::BelongsTo
       class Engine < ::Rails::Engine
-        config.assets.precompile << 'belongs_to_search.js' if config.respond_to? :assets
-        Administrate::Engine.add_javascript 'belongs_to_search.js' if defined?(Administrate::Engine)
+        initializer 'administrate-field-belongs_to_search.add_assets' do |app|
+          app.config.assets.precompile << 'belongs_to_search.js' if app.config.respond_to? :assets
+          Administrate::Engine.add_javascript 'belongs_to_search.js' if defined?(Administrate::Engine)
+        end
       end
 
       def associated_resource_options
